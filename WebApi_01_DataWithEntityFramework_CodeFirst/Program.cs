84927e16721b403c7026d49476f4b91a6532bd40
Karsten Futtrup <kaf@bila.dk>
2022-09-05 15:43:06 +0200

    Initial

--- WebApi_01_DataWithEntityFramework_CodeFirst/Program.cs
+++ WebApi_01_DataWithEntityFramework_CodeFirst/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi_01_DataWithEntityFramework_CodeFirst.Data;
+using WebApi_01_DataWithEntityFramework_CodeFirst.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+Console.WriteLine("Add a db context as a service to be used with dependency injection");
+builder.Services.AddDbContext<PersonDbContext>(opts =>
+    opts.UseSqlServer(builder.Configuration.GetConnectionString("conString")), ServiceLifetime.Singleton);
+
+Console.WriteLine("Use a hosted service to ensure that data exists");
+builder.Services.AddHostedService<DataService>();
 
 var app = builder.Build();
 
